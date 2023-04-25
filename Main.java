cb4e0f5eac9c5882d87a72f0346c076ea6896561
formalyne <formalyne96@gmail.com>
2023-04-25 20:03:22 +0300

    adding Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(56675765, 765, "Ana Vicneanscaia");
+        Integer accountNumber = bankAccount.getAccountNumber();
+        Integer accountBalance = bankAccount.getAccountBalance();
+        String accountHolder = bankAccount.getAccountHolder();
+        System.out.println("This account number is " + accountNumber);
+        System.out.println("This balance has " + accountBalance);
+        System.out.println("Account belongs to " + accountHolder);
+
+    }
+}
+
