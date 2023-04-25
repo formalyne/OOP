04a9d1995eabb6fc76183460c692440b9aed7caf
formalyne <formalyne96@gmail.com>
2023-04-25 21:11:07 +0300

    update of Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
+import Inheritance.Masina;
+import Inheritance.Vehicul;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount(56675765, 765, "Ana Vicneanscaia");
-        Integer accountNumber = bankAccount.getAccountNumber();
-        Integer accountBalance = bankAccount.getAccountBalance();
-        String accountHolder = bankAccount.getAccountHolder();
-        System.out.println("This account number is " + accountNumber);
-        System.out.println("This balance has " + accountBalance);
-        System.out.println("Account belongs to " + accountHolder);
+//        BankAccount bankAccount = new BankAccount(56675765, 765, "Ana Vicneanscaia");
+//        Integer accountNumber = bankAccount.getAccountNumber();
+//        Integer accountBalance = bankAccount.getAccountBalance();
+//        String accountHolder = bankAccount.getAccountHolder();
+//        System.out.println("This account number is " + accountNumber);
+//        System.out.println("This balance has " + accountBalance);
+//        System.out.println("Account belongs to " + accountHolder);
+
+
+        Masina masina = new Masina("BMW");
+        masina.showVehicleDetails();
+
+        Vehicul vehicul  = new Vehicul();
+        vehicul.showVehicleDetails();
 
     }
 }
