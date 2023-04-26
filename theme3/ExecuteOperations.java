dd9683c2a11b026f92efcf7b7e8abb259da99cbc
formalyne <formalyne96@gmail.com>
2023-04-26 21:41:08 +0300

    adding Operations.java and ExecuteOperations.java files

---
+++ theme3/ExecuteOperations.java
@@ -0,0 +1,10 @@
+public class ExecuteOperations extends Operations{
+    public static void main(String[] args) {
+        Operations.printAritmeticOperations();
+        Operations.printRelationarOperations();
+        Operations.printLogicOperations();
+        Operations.useForAndWhile();
+
+        }
+    }
+
